c7b7f47ad39dee355b3143242b863cee39b5407d
TylerSelden <tyler.selden@salesianum.org>
2023-09-29 08:10:57 -0400

    Made scrolling cards (sort of??)

---
+++ public/scripts/home/script.js
@@ -0,0 +1,15 @@
+// So something is... weird... here....
+
+window.onload = function() {
+  window.setInterval(scrollPartners, 5000);
+}
+
+function scrollPartners() {
+  var partners = document.getElementsByClassName("partner");
+  for (var partner of partners) {
+    var partnerIndex = parseInt(partner.classList[1].split("-")[1]); // get number after "partner-"
+    partnerIndex++;
+    if (partnerIndex > 6) partnerIndex = 1;
+    partner.classList.replace(partner.classList[1], `partner-${partnerIndex}`);
+  }
+}
