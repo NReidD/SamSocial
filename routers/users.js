978bbedcf21a68b36e855ceeb74ec829adbd6add
acastellano5 <castellano.andrew05@gmail.com>
2023-10-11 17:49:31 -0400

    finished authentication

--- routers/users.js
+++ routers/users.js
@@ -3,17 +3,22 @@ const router = express.Router()
 const passport = require('passport')
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+const { storeReturnTo } = require('../middleware')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { username, password } = req.body
         const user = new User({username})
         const registeredUser = await User.register(user, password)
-        console.log(registeredUser)
+        req.login(registeredUser, err => {
+            if (err) return next(err)
+            // change redirect to user profile
+            res.redirect('/organizations')
+        })
         // change redirect to user profile
         res.redirect('/organizations')
     } catch (error) {
@@ -26,9 +31,10 @@ router.get('/login', (req, res) => {
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    const redirectUrl = res.locals.returnTo || '/organizations'
     // change to user profile
-    res.redirect('/organizations')
+    res.redirect(redirectUrl)
 })
 
 router.get('/logout', (req, res, next) => {
